94ec04c1932910e5eae03f3bc13e7bff3926b049
Aofusa <5000enPCinAOFUSA@gmail.com>
2024-12-21 03:07:42 +0000

    refactor route

--- src/main.rs
+++ src/main.rs
@@ -4,60 +4,18 @@ mod store;
 
 use std::collections::HashMap;
 use std::ops::Add;
-use std::str::FromStr;
 use std::sync::{Arc, RwLock};
-use std::thread;
 use std::time::{Duration, SystemTime};
 use rsa::RsaPrivateKey;
-use serde_derive::{Deserialize, Serialize};
-use tokio::time::sleep;
-use tracing::{debug, info};
+use tracing::info;
 use tracing_subscriber;
-use url::Url;
-use utoipa::{OpenApi, ToSchema};
+use utoipa::OpenApi;
 use utoipa_swagger_ui::Config;
 use uuid::Uuid;
 use warp::{Filter, Rejection, Reply};
-use warp::host::Authority;
 use warp::http::{Response, StatusCode, Uri};
 use warp::path::{FullPath, Tail};
-use crate::sign_url::SignUrlContainer;
-use crate::account::Account;
 use crate::store::DataStore;
-
-#[derive(Deserialize, Serialize, Debug)]
-struct CreateQuery {
-    expires: Option<u64>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct CreateResponse {
-    sign_url: String,  // ?payload=...&expires=...&signature=...
-}
-
-impl CreateResponse {
-    fn new(base: String, sign: &SignUrlContainer) -> Self {
-        let mut url = Url::parse(&base).unwrap().join("verify").unwrap();
-        url.set_query(Some(
-            &format!("payload={}&expires={}&signature={}",
-                 urlencoding::encode(sign.payload.as_str()).to_string(),
-                 sign.expires,
-                 urlencoding::encode(sign.signature.as_str()).to_string()
-            )
-        ));
-        let sign_url = url.to_string();
-        CreateResponse {
-            sign_url
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct VerifyQuery {
-    payload: String,
-    expires: u64,
-    signature: String,
-}
 
 struct ServerSession {
     id: Uuid,
@@ -162,65 +120,69 @@ mod filter {
     }
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
-    tracing_subscriber::fmt()
-      .with_env_filter(filter)
-      .init();
+mod route {
+    use std::collections::HashMap;
+    use std::str::FromStr;
+    use std::sync::{Arc, RwLock};
+    use std::thread;
+    use std::time::{Duration, SystemTime};
+    use rsa::RsaPrivateKey;
+    use serde_derive::{Deserialize, Serialize};
+    use tokio::time::sleep;
+    use tracing::{debug, info};
+    use url::Url;
+    use utoipa::OpenApi;
+    use utoipa_swagger_ui::Config;
+    use uuid::Uuid;
+    use warp::{Filter, Rejection, Reply};
+    use warp::host::Authority;
+    use warp::http::Response;
+    use warp::reply::Json;
+    use crate::{filter, serve_swagger, ApiDoc, ServerSession};
+    use crate::account::Account;
+    use crate::sign_url::SignUrlContainer;
+    use crate::store::DataStore;
 
-    const DEFAULT_RSA_BIT_SIZE: usize = 2048;
-    const DEFAULT_DATABASE_CONNECTION: u32 = 1024;
     const DEFAULT_CONTENT_LENGTH_LIMIT: u64 = 1024 * 16;
     const DEFAULT_COOKIE_EXPIRES: u64 = 2592000;  // 2592000秒 = 30日
     const DEFAULT_VERIFY_EXPIRES: u64 = 600000;  // 600000ミリ秒 = 10分
 
-    // 2048bit = 512byte まで暗号化可能
-    // password: 97byte 固定
-    // padding: 11byte 固定
-    // username = 512-97-11 = 408文字 (255文字の制限を付けるならこれでOK 1023文字なら鍵長が8192bit必要)
-    info!("start create encrypt key...");
-    let private_key = Arc::new(RsaPrivateKey::new(&mut rand::thread_rng(), DEFAULT_RSA_BIT_SIZE)?);
-    info!("finish create encrypt key");
+    #[derive(Deserialize, Serialize, Debug)]
+    struct CreateQuery {
+        expires: Option<u64>,
+    }
 
-    info!("start setup datastore...");
-    let datastore = Arc::new(DataStore::setup(DEFAULT_DATABASE_CONNECTION).await?);
-    info!("finish setup datastore...");
+    #[derive(Deserialize, Serialize, Debug)]
+    struct CreateResponse {
+        sign_url: String,  // ?payload=...&expires=...&signature=...
+    }
 
-    let session = Arc::new(RwLock::new(HashMap::<Uuid, Arc<RwLock<ServerSession>>>::new()));
+    impl CreateResponse {
+        fn new(base: String, sign: &SignUrlContainer) -> Self {
+            let mut url = Url::parse(&base).unwrap().join("verify").unwrap();
+            url.set_query(Some(
+                &format!("payload={}&expires={}&signature={}",
+                         urlencoding::encode(sign.payload.as_str()).to_string(),
+                         sign.expires,
+                         urlencoding::encode(sign.signature.as_str()).to_string()
+                )
+            ));
+            let sign_url = url.to_string();
+            CreateResponse {
+                sign_url
+            }
+        }
+    }
 
-    /*
-    post /create?expires=number username,password
-     -> res signedurl
-    get /verify?payload=...&expires=...&signature=... → 冪等性 アカウント登録完了
+    #[derive(Serialize, Deserialize, Debug)]
+    struct VerifyQuery {
+        payload: String,
+        expires: u64,
+        signature: String,
+    }
 
-    get /health-check
-    post /login username,password
-    get /logout
-    get /protected
-    */
-
-    // GET /openapi.json
-    let openapi_json = warp::path::path("openapi.json")
-      .and(warp::get())
-      .map(|| warp::reply::json(&ApiDoc::openapi()));
-
-    // GET /docs
-    let config = Arc::new(Config::from("/openapi.json"));
-    let swagger_ui = warp::path::path("swagger-ui")
-      .and(warp::get())
-      .and(warp::path::full())
-      .and(warp::path::tail())
-      .and(warp::any().map(move || config.clone()))
-      .and_then(serve_swagger);
-
-    // GET /health-check
-    let health_check = warp::path::path("health-check")
-      .and(warp::get())
-      .map(|| "Hello, World!");
-
-    // GET /
-    let index = {
+    pub fn index() -> impl Filter<Extract = (warp::fs::File,), Error = Rejection> + Clone {
+        // GET /
         let path = warp::path::end()
           .and(warp::get());
         #[cfg(debug_assertions)]
@@ -228,10 +190,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         #[cfg(not(debug_assertions))]
         let api = path.map(|| warp::reply::html(include_str!("../asset/index.html")));
         api
-    };
+    }
 
-    // POST /login
-    let login = {
+    pub fn health_check() -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
+        // GET /health-check
+        warp::path::path("health-check")
+          .and(warp::get())
+          .map(|| "Hello, World!".to_string())
+    }
+
+    pub fn login(datastore: Arc<DataStore>, session: Arc<RwLock<HashMap<Uuid, Arc<RwLock<ServerSession>>>>>) -> impl Filter<Extract = (Result<Response<String>, warp::http::Error>,), Error = Rejection> + Clone {
+        // POST /login
         let datastore = datastore.clone();
         let session = session.clone();
         warp::path::path("login")
@@ -270,10 +239,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                   }
               }
           })
-    };
+    }
 
-    // GET /logout
-    let logout = {
+    pub fn logout(session: Arc<RwLock<HashMap<Uuid, Arc<RwLock<ServerSession>>>>>) -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
+        // GET /logout
         let session = session.clone();
         warp::path::path("logout")
           .and(warp::get())
@@ -291,10 +260,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                   "logout".to_string()
               }
           })
-    };
+    }
 
-    // GET /protected
-    let protected = {
+    pub fn protected(session: Arc<RwLock<HashMap<Uuid, Arc<RwLock<ServerSession>>>>>) -> impl Filter<Extract = (Result<Response<String>, warp::http::Error>,), Error = Rejection> + Clone {
+        // GET /protected
         let session = session.clone();
         warp::path::path("protected")
           .and(warp::get())
@@ -326,10 +295,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .body("unauthorized".to_string())
               }
           })
-    };
+    }
 
-    // POST /create?expires=number
-    let create = {
+    pub fn create(datastore: Arc<DataStore>, private_key: Arc<RsaPrivateKey>) -> impl Filter<Extract = (Json,), Error = Rejection> + Clone {
+        // POST /create?expires=number
         let private_key = private_key.clone();
         let datastore = datastore.clone();
         warp::path("create")
@@ -395,10 +364,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                   }
               }
           })
-    };
+    }
 
-    // GET /verify?signature~~~
-    let verify = {
+    pub fn verify(datastore: Arc<DataStore>, session: Arc<RwLock<HashMap<Uuid, Arc<RwLock<ServerSession>>>>>, private_key: Arc<RsaPrivateKey>) -> impl Filter<Extract = (Result<Response<String>, warp::http::Error>,), Error = Rejection> + Clone {
+        // GET /verify?signature~~~
         let private_key = private_key.clone();
         let datastore = datastore.clone();
         let session = session.clone();
@@ -475,17 +444,61 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                   }
               }
           })
-    };
+    }
 
-    let routes = health_check
-      .or(index)
-      .or(create)
-      .or(verify)
-      .or(login)
-      .or(logout)
-      .or(protected)
-      .or(openapi_json)
-      .or(swagger_ui)
+    pub fn openapi_json() -> impl Filter<Extract = (Json,), Error = Rejection> + Clone {
+        // GET /openapi.json
+        warp::path::path("openapi.json")
+          .and(warp::get())
+          .map(|| warp::reply::json(&ApiDoc::openapi()))
+    }
+
+    pub fn swagger_ui() -> impl Filter<Extract = (Box<dyn Reply + 'static>,), Error = Rejection> + Clone {
+        // GET /docs
+        let config = Arc::new(Config::from("/openapi.json"));
+        warp::path::path("swagger-ui")
+          .and(warp::get())
+          .and(warp::path::full())
+          .and(warp::path::tail())
+          .and(warp::any().map(move || config.clone()))
+          .and_then(serve_swagger)
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
+    tracing_subscriber::fmt()
+      .with_env_filter(filter)
+      .init();
+
+    const DEFAULT_RSA_BIT_SIZE: usize = 2048;
+    const DEFAULT_DATABASE_CONNECTION: u32 = 1024;
+
+    // 2048bit = 512byte まで暗号化可能
+    // password: 97byte 固定
+    // padding: 11byte 固定
+    // username = 512-97-11 = 408文字 (255文字の制限を付けるならこれでOK 1023文字なら鍵長が8192bit必要)
+    info!("start create encrypt key...");
+    let private_key = Arc::new(RsaPrivateKey::new(&mut rand::thread_rng(), DEFAULT_RSA_BIT_SIZE)?);
+    info!("finish create encrypt key");
+
+    info!("start setup datastore...");
+    let datastore = Arc::new(DataStore::setup(DEFAULT_DATABASE_CONNECTION).await?);
+    info!("finish setup datastore...");
+
+    let session = Arc::new(RwLock::new(HashMap::<Uuid, Arc<RwLock<ServerSession>>>::new()));
+
+    let routes =
+      route::health_check()
+        .or(route::index())
+        .or(route::create(datastore.clone(), private_key.clone()))
+        .or(route::verify(datastore.clone(), session.clone(), private_key.clone()))
+        .or(route::login(datastore.clone(), session.clone()))
+        .or(route::logout(session.clone()))
+        .or(route::protected(session.clone()))
+        .or(route::openapi_json())
+        .or(route::swagger_ui())
     ;
 
     let non_tls_server = warp::serve(routes.clone().with(warp::trace::request()))
